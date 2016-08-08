54adb30c37ef830854753b075f57510434584b7c
acoshift <acoshift@gmail.com>
2016-08-08 21:28:22 +0700

    Remove Auth Service

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -1,11 +1,8 @@
 import angular from 'angular'
 
-import AuthService from './auth.service'
-
 const module = 'app.services'
 
 angular
   .module(module, [])
-  .service('AuthService', AuthService)
 
 export default module
