13d526b1d7a7c15242a73015ffa85de369e67667
acoshift <acoshift@gmail.com>
2016-08-08 23:09:59 +0700

    Cleanup

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,12 +1,10 @@
 import angular from 'angular'
 
-import Config from './config'
 import Home from './home/home'
 import About from './about/about'
 
 export default angular
   .module('app.pages', [])
-  .config(Config)
   .component(...Home)
   .component(...About)
   .name
