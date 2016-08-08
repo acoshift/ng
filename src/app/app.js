8af3955dbc4180eebbb92236142fb98490b03843
acoshift <acoshift@gmail.com>
2016-08-08 18:37:50 +0700

    Refactor

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,17 @@
-import './module'
+import angular from 'angular'
+
+import ServiceModule from './service'
+
+import Config from './config'
+import Run from './Run'
+import AppController from './app.controller'
+
+angular
+  .module('app', [
+    'ui.router',
+
+    ServiceModule
+  ])
+  .config(Config)
+  .Run(Run)
+  .controller('AppController', AppController)
--- src/app/service/index.js
+++ src/app/service/index.js
@@ -1,8 +1,11 @@
 import angular from 'angular'
 
+import AuthService from './auth.service'
+
 const module = 'app.service'
 
 angular
   .module('module')
+  .service('AuthService', AuthService)
 
 export default module
